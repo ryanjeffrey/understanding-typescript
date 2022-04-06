1f225f49716a9423dc75f961a8997286a6a89303
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-04-06 10:39:37 -0500

    Define num1 and num2 as numbers

--- first-project/using-ts.ts
+++ first-project/using-ts.ts
@@ -2,7 +2,7 @@ const button = document.querySelector("button");
 const input1 = document.getElementById("num1")! as HTMLInputElement;
 const input2 = document.getElementById("num2")! as HTMLInputElement;
 
-function add(num1, num2) {
+function add(num1: number, num2: number) {
   return num1 + num2;
 }
 
