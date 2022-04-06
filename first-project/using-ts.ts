3acb9b0026103daab2895cad02751c3087827c14
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-04-06 10:38:23 -0500

    Typecast constants as HTML Inputs

--- first-project/using-ts.ts
+++ first-project/using-ts.ts
@@ -1,6 +1,6 @@
 const button = document.querySelector("button");
-const input1 = document.getElementById("num1");
-const input2 = document.getElementById("num2");
+const input1 = document.getElementById("num1")! as HTMLInputElement;
+const input2 = document.getElementById("num2")! as HTMLInputElement;
 
 function add(num1, num2) {
   return num1 + num2;
