edb9b7a74d1a65eedeb39560e783b86490bd820d
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-04-13 11:54:36 -0500

    Create for loop to get hobbies, compile

--- second-project/app.js
+++ second-project/app.js
@@ -3,4 +3,8 @@ var person = {
     age: 34,
     hobbies: ['running', 'Chicago Bears']
 };
-console.log(person.hobbies);
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
--- second-project/app.ts
+++ second-project/app.ts
@@ -4,4 +4,8 @@ const person = {
     hobbies: ['running', 'Chicago Bears']
 };
 
-console.log(person.hobbies);
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase())
+}
