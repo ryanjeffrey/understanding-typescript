352840b5ea9f54f892503b2278bbe03e9e91f688
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-04-13 11:15:01 -0500

    Add resultPhrase to result, compile

--- second-project/app.js
+++ second-project/app.js
@@ -1,7 +1,6 @@
-function add(n1, n2, showResult) {
-    return n1 + n2;
+function add(n1, n2, showResult, phrase) {
     if (showResult) {
-        console.log(n1 + n2);
+        console.log(phrase + n1 + n2);
     }
     else {
         return n1 + n2;
@@ -10,4 +9,5 @@ function add(n1, n2, showResult) {
 var number1 = 5;
 var number2 = 2.8;
 var printResult = true;
-add(number1, number2, printResult);
+var resultPhrase = 'Result is: ';
+add(number1, number2, printResult, resultPhrase);
