3c1bb88c9c5bda9c9a12a4fca59d15310cbd4c2c
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-04-14 14:21:15 -0500

    Push new values to role

--- second-project/app.js
+++ second-project/app.js
@@ -4,6 +4,8 @@ var person = {
     hobbies: ['running', 'Chicago Bears'],
     role: [2, 'author']
 };
+person.role.push('admin');
+person.role[1] = 10;
 console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
--- second-project/app.ts
+++ second-project/app.ts
@@ -5,6 +5,9 @@ const person = {
     role: [2, 'author']
 };
 
+person.role.push('admin');
+person.role[1] = 10;
+
 console.log(person.name);
 
 for (const hobby of person.hobbies) {
