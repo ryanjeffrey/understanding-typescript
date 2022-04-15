3e2cba62317aaadbd1d110e4929e10d8d86e0356
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-04-15 10:39:39 -0500

    Else concatenate the strings

--- second-project/app.js
+++ second-project/app.js
@@ -3,6 +3,9 @@ function combine(input1, input2) {
     if (typeof input1 === 'number' && typeof input2 === 'number') {
         result = input1 + input2;
     }
+    else {
+        result = input1.toString() + input2.toString();
+    }
     return result;
 }
 var combinedAges = combine(30, 63);
