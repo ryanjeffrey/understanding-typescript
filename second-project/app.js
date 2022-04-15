d9de04893b366c4451c3444411d45b82a23c9436
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-04-15 09:51:20 -0500

    Comment out implicit type assigment and all code besides the person constant, compile

--- second-project/app.js
+++ second-project/app.js
@@ -1,13 +1,24 @@
+// // Implicit type assignment
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string];
+// } = {
+//     name: 'Ryan',
+//     age: 34,
+//     hobbies: ['running', 'Chicago Bears'],
+//     role: [2, 'author']
+// };
 var person = {
-    name: 'Ryan',
-    age: 34,
+    name: 'Maximilian',
+    age: 30,
     hobbies: ['running', 'Chicago Bears'],
-    role: [2, 'author']
+    role: ADMIN
 };
-person.role.push('admin');
-person.role[1] = 10;
-console.log(person.name);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toUpperCase());
-}
+// person.role.push('admin');
+// person.role[0] = 10;
+// console.log(person.name);
+// for (const hobby of person.hobbies) {
+//     console.log(hobby.toUpperCase())
+// }
--- second-project/app.ts
+++ second-project/app.ts
@@ -1,20 +1,30 @@
-const person: {
-    name: string;
-    age: number;
-    hobbies: string[];
-    role: [number, string];
-} = {
-    name: 'Ryan',
-    age: 34,
+// // Implicit type assignment
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string];
+// } = {
+//     name: 'Ryan',
+//     age: 34,
+//     hobbies: ['running', 'Chicago Bears'],
+//     role: [2, 'author']
+// };
+
+const person = {
+    name: 'Maximilian',
+    age: 30,
     hobbies: ['running', 'Chicago Bears'],
-    role: [2, 'author']
+    role: ADMIN
 };
 
-person.role.push('admin');
-person.role[0] = 10;
 
-console.log(person.name);
 
-for (const hobby of person.hobbies) {
-    console.log(hobby.toUpperCase())
-}
+// person.role.push('admin');
+// person.role[0] = 10;
+
+// console.log(person.name);
+
+// for (const hobby of person.hobbies) {
+//     console.log(hobby.toUpperCase())
+// }
