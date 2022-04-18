37cee1829cbda7aaa087bf251b15320d3467386d
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-04-18 10:32:36 -0500

    Set type of resultConversion and add input numbers

--- second-project/app.js
+++ second-project/app.js
@@ -1,17 +1,16 @@
 function combine(input1, input2, resultConversion) {
     var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+        result = +input1 + +input2;
     }
     else {
         result = input1.toString() + input2.toString();
     }
-    if (resultConversion === 'as-number') {
-        return +result;
-    }
-    else {
-        return result.toString();
-    }
+    // if (resultConversion === 'as-number') {
+    //     return +result;
+    // } else {
+    //     return result.toString();
+    // }
 }
 var combinedAges = combine(30, 63, 'as-number');
 console.log(combinedAges);
--- second-project/app.ts
+++ second-project/app.ts
@@ -1,15 +1,16 @@
 function combine(input1: number | string, input2: number | string, resultConversion: string) {
     let result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+        result = +input1 + +input2;
     } else {
         result = input1.toString() + input2.toString();
     }
-    if (resultConversion === 'as-number') {
-        return +result;
-    } else {
-        return result.toString();
-    }
+
+    // if (resultConversion === 'as-number') {
+    //     return +result;
+    // } else {
+    //     return result.toString();
+    // }
     
 }
 
