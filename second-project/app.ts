bc3eb8bed24aa4273d6aab6bd97639a1a73220e3
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-04-20 09:35:39 -0500

    Create type alias

--- second-project/app.ts
+++ second-project/app.ts
@@ -1,3 +1,5 @@
+type Combinable = number | string;
+
 function combine(input1: number | string, input2: number | string, resultConversion: 'as-number' | 'as-text') {
     let result;
     if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
