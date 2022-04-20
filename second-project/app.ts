de3b0f1080f877d5a605150e581bb87f2d10df80
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-04-20 09:37:12 -0500

    Apply type alias to inputs

--- second-project/app.ts
+++ second-project/app.ts
@@ -1,6 +1,9 @@
 type Combinable = number | string;
 
-function combine(input1: number | string, input2: number | string, resultConversion: 'as-number' | 'as-text') {
+function combine(
+    input1: Combinable, 
+    input2: Combinable, 
+    resultConversion: 'as-number' | 'as-text') {
     let result;
     if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
         result = +input1 + +input2;
