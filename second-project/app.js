7f4ccaa43275e364166f212c9e684daa32309b26
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-04-20 10:31:42 -0500

    Write addAndHandle function that includes callback function, compile

--- second-project/app.js
+++ second-project/app.js
@@ -6,6 +6,13 @@ function printResult(num) {
     console.log('Result: ' + num);
 }
 printResult(add(5, 12));
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
+addAndHandle(10, 20, function (result) {
+    console.log(result);
+});
 var combineValues;
 combineValues = add;
 console.log(combineValues(8, 8));
--- second-project/app.ts
+++ second-project/app.ts
@@ -9,6 +9,15 @@ function printResult(num: number): void {
 
 printResult(add(5, 12));
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+    const result = n1 + n2;
+    cb(result);
+}
+
+addAndHandle(10, 20, (result) => {
+    console.log(result);
+});
+
 let combineValues: (a: number, b: number) => number;
 
 combineValues = add;
