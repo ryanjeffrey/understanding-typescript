1ec39ee3b2dcd73e45767beb6970e583583f83dd
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-04-27 13:50:29 -0500

    Compile all .ts files

---
+++ second-project/objects-arrays-enums.js
@@ -0,0 +1,36 @@
+"use strict";
+// // Explicit type assignment
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string];
+// } = {
+//     name: 'Ryan',
+//     age: 34,
+//     hobbies: ['running', 'Chicago Bears'],
+//     role: [2, 'author']
+// };
+// Enum
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+;
+const person = {
+    name: 'Maximilian',
+    age: 30,
+    hobbies: ['running', 'Chicago Bears'],
+    role: Role.ADMIN
+};
+if (person.role === Role.AUTHOR) {
+    console.log('is author');
+}
+// person.role.push('admin');
+// person.role[0] = 10;
+// console.log(person.name);
+// for (const hobby of person.hobbies) {
+//     console.log(hobby.toUpperCase())
+// }
---
+++ second-project/unknown-never.js
@@ -0,0 +1,13 @@
+"use strict";
+let userInput;
+let userName;
+userInput = 5;
+userInput = 'JF';
+// 'unknown' type is more restrictive than 'any' type
+if (typeof userInput === 'string') {
+    userName = userInput;
+}
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+}
+generateError('An error occurred!', 500);
