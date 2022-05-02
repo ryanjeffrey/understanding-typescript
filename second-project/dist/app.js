610752b808dc201df69e42f637aee3ca38c464d2
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-05-02 11:32:07 -0500

    Write a clickHandler function that logs a message using the bind method

--- second-project/dist/app.js
+++ second-project/dist/app.js
@@ -1,5 +1,6 @@
 "use strict";
 const button = document.querySelector('button');
-button.addEventListener('click', () => {
-    console.log('Clicked!');
-});
+function clickHandler(message) {
+    console.log('Clicked! ' + message);
+}
+button.addEventListener('click', clickHandler.bind(null, "You're Welcome!"));
--- second-project/src/app.ts
+++ second-project/src/app.ts
@@ -1,5 +1,7 @@
 const button = document.querySelector('button')!;
 
-button.addEventListener('click', () => {
-    console.log('Clicked!')
-});
+function clickHandler(message: string) {
+    console.log('Clicked! ' + message)
+}
+
+button.addEventListener('click', clickHandler.bind(null, "You're Welcome!"));
