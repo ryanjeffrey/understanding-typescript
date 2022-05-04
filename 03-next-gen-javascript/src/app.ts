e4ec64f93a112dda6e66e37c79b2bf396584873c
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-05-04 11:58:29 -0500

    Write a const that uses an arrow function

--- 03-next-gen-javascript/src/app.ts
+++ 03-next-gen-javascript/src/app.ts
@@ -4,13 +4,19 @@ let age = 34;
 
 age = 30;
 
-function add(a: number, b: number) {
-    let result;
-    result = a + b;
-    return result;
-}
+// function add(a: number, b: number) {
+//     let result;
+//     result = a + b;
+//     return result;
+// }
 
-if (age > 20) {
-    let isOld = true;
-    console.log(isOld);
-}
+// if (age > 20) {
+//     let isOld = true;
+//     console.log(isOld);
+// }
+
+const add = (a: number, b: number) => {
+    return a + b;
+};
+
+console.log(add(2, 5));
