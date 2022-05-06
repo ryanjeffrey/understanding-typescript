9b0be0efe912656dd6b799682d7d671bdbe82161
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-05-06 13:32:55 -0500

    Create an arrow function that outputs a union type

--- 03-next-gen-javascript/src/app.ts
+++ 03-next-gen-javascript/src/app.ts
@@ -17,4 +17,6 @@ age = 30;
 
 const add = (a: number, b: number) =>  a + b;
 
-console.log(add(2, 5));
+const printOutput = (output: string | number) => console.log(output);
+
+printOutput(add(2, 5));
