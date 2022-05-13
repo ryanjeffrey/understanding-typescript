527b3eb58c8245ab9939c6a9bdbd9bb80bed6fe7
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-05-13 11:20:50 -0500

    Create a describe method that logs the department name, call method and compile

--- 04-classes-and-interfaces/dist/app.js
+++ 04-classes-and-interfaces/dist/app.js
@@ -3,7 +3,10 @@ class Department {
     constructor(n) {
         this.name = n;
     }
+    describe() {
+        console.log('Department: ' + this.name);
+    }
 }
 const accounting = new Department('Accounting');
-console.log(accounting);
+accounting.describe();
 //# sourceMappingURL=app.js.map
