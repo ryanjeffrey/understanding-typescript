8981cd07a502a61bad13e4960ebdca4b2394cb09
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-05-18 14:46:33 -0500

    Create user1 based on interface

--- 04-classes-and-interfaces/dist/app.js
+++ 04-classes-and-interfaces/dist/app.js
@@ -1,2 +1,11 @@
 "use strict";
+let user1;
+user1 = {
+    name: 'Ryan',
+    age: 34,
+    greet(phrase) {
+        console.log(phrase + ' ' + this.name);
+    }
+};
+user1.greet('Hi there - I am');
 //# sourceMappingURL=app.js.map
