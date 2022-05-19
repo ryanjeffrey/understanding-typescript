83303c13d649fb8003ffc9b6589be63ab8c4ed27
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-05-19 16:24:21 -0500

    Change interface to Greetable and create Person class that implements it

--- 04-classes-and-interfaces/dist/app.js
+++ 04-classes-and-interfaces/dist/app.js
@@ -1,11 +1,14 @@
 "use strict";
-let user1;
-user1 = {
-    name: 'Ryan',
-    age: 34,
-    greet(phrase) {
-        console.log(phrase + ' ' + this.name);
+class Person {
+    constructor(n) {
+        this.age = 34;
+        this.name = n;
     }
-};
+    greet(phrase) {
+        console.log(phrase + " " + this.name);
+    }
+}
+let user1;
+user1 = new Person('Ryan');
 user1.greet('Hi there - I am');
 //# sourceMappingURL=app.js.map
