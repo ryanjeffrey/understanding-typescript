0851e68e9801f294bc59b364b9cf931c4e23b4f2
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-06-06 14:52:36 -0500

    Create Person class and instantiate

--- 07-decorators/dist/app.js
+++ 07-decorators/dist/app.js
@@ -1,2 +1,10 @@
 "use strict";
+class Person {
+    constructor() {
+        this.name = 'Darnell';
+        console.log('Creating person object...');
+    }
+}
+const pers = new Person();
+console.log(pers);
 //# sourceMappingURL=app.js.map
---
+++ 07-decorators/src/app.ts
@@ -0,0 +1,11 @@
+class Person {
+    name = 'Darnell';
+
+    constructor() {
+        console.log('Creating person object...')
+    }
+}
+
+const pers = new Person()
+
+console.log(pers)
