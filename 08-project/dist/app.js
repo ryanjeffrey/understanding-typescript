495cf88e1186f260b093525f4cac3834bba1ca7f
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-06-13 15:07:32 -0500

    Assign input elements with querySelector, prettier formatting

--- 08-project/dist/app.js
+++ 08-project/dist/app.js
@@ -1,15 +1,18 @@
 "use strict";
 class ProjectInput {
     constructor() {
-        this.templateElement = document.getElementById('project-input');
-        this.hostElement = document.getElementById('app');
+        this.templateElement = document.getElementById("project-input");
+        this.hostElement = document.getElementById("app");
         const importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild;
-        this.element.id = 'user-input';
+        this.element.id = "user-input";
+        this.titleInputElement = this.element.querySelector("#title");
+        this.descriptionInputElement = this.element.querySelector("#description");
+        this.peopleInputElement = this.element.querySelector("#people");
         this.attach();
     }
     attach() {
-        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+        this.hostElement.insertAdjacentElement("afterbegin", this.element);
     }
 }
 const projectInput = new ProjectInput();
