ba537aa7bda5fc50322df3dabca1753aaa3abc9d
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-06-13 13:59:20 -0500

    Create ProjectInput class and identify templateElement and hostElement

--- 08-project/dist/app.js
+++ 08-project/dist/app.js
@@ -1,2 +1,8 @@
 "use strict";
+class ProjectInput {
+    constructor() {
+        this.templateElement = document.getElementById('project-input');
+        this.hostElement = document.getElementById('app');
+    }
+}
 //# sourceMappingURL=app.js.map
