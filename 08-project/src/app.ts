219fafe6412898c5472264687d8bb1579571e23e
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-06-16 10:39:21 -0500

    Create autobind decorator and apply it to submitHandler

--- 08-project/src/app.ts
+++ 08-project/src/app.ts
@@ -1,3 +1,20 @@
+// Autobind decorator
+function autobind(
+  _: any, 
+  _2: string, 
+  descriptor: PropertyDescriptor
+  ) {
+    const originalMethod = descriptor.value;
+    const adjustedDescriptor: PropertyDescriptor = {
+      configurable: true,
+      get() {
+        const boundFunction = originalMethod.bind(this);
+        return boundFunction;
+      }
+    }
+    return adjustedDescriptor;
+  }
+
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
@@ -27,13 +44,14 @@ class ProjectInput {
     this.attach();
   }
 
+  @autobind
   private submitHandler(event: Event) {
     event.preventDefault();
     console.log(this.titleInputElement.value);
   }
 
   private configure() {
-    this.element.addEventListener('submit', this.submitHandler.bind(this));
+    this.element.addEventListener('submit', this.submitHandler);
   }
 
   private attach() {
