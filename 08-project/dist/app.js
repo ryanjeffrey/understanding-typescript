c842e86288862ef7282780c367a482688c4b1201
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-06-27 10:18:23 -0500

    Update renderContent method with targeted elements

--- 08-project/dist/app.js
+++ 08-project/dist/app.js
@@ -99,6 +99,20 @@ class Component {
         this.hostElement.insertAdjacentElement(insertAtStart ? "afterbegin" : "beforeend", this.element);
     }
 }
+class ProjectItem extends Component {
+    constructor(hostId, project) {
+        super("single-project", hostId, false, project.id);
+        this.project = project;
+        this.configure();
+        this.renderContent();
+    }
+    configure() { }
+    renderContent() {
+        this.element.querySelector("h2").textContent = this.project.title;
+        this.element.querySelector("h3").textContent = this.project.people.toString();
+        this.element.querySelector("p").textContent = this.project.description;
+    }
+}
 class ProjectList extends Component {
     constructor(type) {
         super("project-list", "app", false, `${type}-projects`);
