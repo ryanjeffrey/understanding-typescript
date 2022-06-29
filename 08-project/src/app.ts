49c3c3a5a20911662cefb0abbe9ae53785c573a4
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-06-29 11:53:09 -0500

    Create subfolder for util, move validation to it and update reference path

--- 08-project/src/app.ts
+++ 08-project/src/app.ts
@@ -1,7 +1,7 @@
 /// <reference path="./models/drag-drop.ts" />
 /// <reference path="./models/project.ts" />
 /// <reference path="project-state.ts" />
-/// <reference path="validation.ts" />
+/// <reference path="./util/validation.ts" />
 /// <reference path="./decorators/autobind.ts" />
 
 namespace App {
