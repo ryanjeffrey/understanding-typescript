ffa9f12378df60d6f229b795493d1b40e0a3b512
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-07-15 11:48:24 -0500

    Update module to ES2015, update script type in HTML, comment out outFile in tsconfig, and compile

---
+++ 08-project/dist/decorators/autobind.js
@@ -0,0 +1,12 @@
+export function autobind(_, _2, descriptor) {
+    const originalMethod = descriptor.value;
+    const adjustedDescriptor = {
+        configurable: true,
+        get() {
+            const boundFunction = originalMethod.bind(this);
+            return boundFunction;
+        },
+    };
+    return adjustedDescriptor;
+}
+//# sourceMappingURL=autobind.js.map
